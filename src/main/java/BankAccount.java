e607bbe94dd64b1c51973fbdae023b962faa3f89
amelieboydshire <aboydshire10@gmail.com>
2023-07-14 22:30:41 +0100

    getters and setters

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,4 +1,50 @@
+import java.time.LocalDate;
+
 public class BankAccount {
 
-    public
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String accountNumber;
+    private Double balance;
+
+    public void setFirstName(String inputFirstName) {
+        this.firstName = inputFirstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String inputLastName) {
+        this.lastName = inputLastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
 }
